f452baba66692ea98353c5d48229f74c058931be
Bauticasañas <bauticasanas@gmail.com>
2022-02-09 13:20:01 -0300

    Agregando

--- src/componentes/CartWidget/CartWidget.js
+++ src/componentes/CartWidget/CartWidget.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './CartWidget.css';
-
+import {Link} from "react-router-dom";
 
 
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
@@ -12,7 +12,7 @@ const CartWidget = () => {
 
   
   return <div className='cartWidget'>
-  <p>0</p><ShoppingCartOutlinedIcon/>
+  <p>0</p><Link  to={"/Cart"}><ShoppingCartOutlinedIcon/></Link>
   <Login/>
   </div>;
 };
